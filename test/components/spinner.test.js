e80dfa2575f4980fb7a00bd9dfb9eab887088ef5
Kevin Lin <developer@kevinlin.info>
2017-08-06 13:52:37 -0700

    Spinner test

---
+++ test/components/spinner.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import sinon from 'sinon';
+import { mount } from 'enzyme';
+import Spinner from 'components/spinner';
+import { colors } from 'styles/color';
+
+describe('Spinner', () => {
+  test('Standard rendering', () => {
+    const spinner = mount(
+      <Spinner />,
+    );
+
+    expect(spinner.find('div').length).toBe(1);
+    expect(spinner.find('div').props().style.backgroundColor).toBeDefined();
+  });
+
+  test('Color pulsation', () => {
+    const clock = sinon.useFakeTimers();
+    const spinner = mount(
+      <Spinner color={colors.red} />,
+    );
+
+    expect(spinner.state().color).toBe(colors.gray5);
+    clock.tick(600);
+    expect(spinner.state().color).toBe(colors.red);
+    clock.tick(600);
+    expect(spinner.state().color).toBe(colors.gray5);
+
+    spinner.unmount();
+
+    clock.restore();
+  });
+});
