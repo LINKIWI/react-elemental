71828796d5179531c921515dd2a5718c913c31d2
Kevin Lin <developer@kevinlin.info>
2017-08-31 16:42:18 -0500

    Toast test

---
+++ test/components/toast.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import Spacing from 'components/spacing';
+import Toast from 'components/toast';
+
+describe('Toast', () => {
+  test('Accepts proxy props', () => {
+    const onClick = jest.fn();
+    const toast = mount(
+      <Toast onClick={onClick} />,
+    );
+
+    expect(toast.at(0).props().onClick).toBe(onClick);
+  });
+
+  test('Basic rendering', () => {
+    const toast = shallow(
+      <Toast>
+        children
+      </Toast>,
+    );
+
+    expect(toast.find(Spacing).last().children().text()).toBe('children');
+  });
+});
