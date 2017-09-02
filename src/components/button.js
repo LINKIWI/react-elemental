e3b4af3a109badb398539415307af1538e8ab4ee
Kevin Lin <developer@kevinlin.info>
2017-09-02 10:01:34 -0500

    Move RGB parsing functions to util file

--- src/components/button.js
+++ src/components/button.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Text from 'components/text';
 import { colors } from 'styles/color';
+import { parseHexToRGB, rgbToHex } from 'util/color';
 
 const noop = () => {};
 
@@ -11,29 +12,6 @@ const ACTIVE_INTENSITY_RATIO = 0.95;
 const STATE_IDLE = 'idle';
 const STATE_HOVER = 'hover';
 const STATE_ACTIVE = 'active';
-
-/**
- * Parse an RGB hex string into individual red, green, and blue components.
- *
- * @param {string} hex Color hex string, e.g. '#aabbcc'
- * @returns {Array} Array of [red, green, blue] integers in the range [0, 255].
- */
-const parseHexToRGB = (hex) => {
-  const red = hex.substring(1, 3);
-  const green = hex.substring(3, 5);
-  const blue = hex.substring(5, 7);
-
-  return [red, green, blue].map((component) => parseInt(component, 16));
-};
-
-/**
- * Construct an RGB hex string from individual red, green, blue integer values.
- *
- * @param {Array} Array of [red, green, blue] component values, each in range [0 255].
- * @returns {string} RGB hex string for the specified input.
- */
-const rgbToHex = ([red, green, blue]) =>
-  `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 
 // Mapping of button sizes to the corresponding default text size.
 const textSizeMap = {
---
+++ src/util/color.js
@@ -0,0 +1,22 @@
+/**
+ * Parse an RGB hex string into individual red, green, and blue components.
+ *
+ * @param {string} hex Color hex string, e.g. '#aabbcc'
+ * @returns {Array} Array of [red, green, blue] integers in the range [0, 255].
+ */
+export const parseHexToRGB = (hex) => {
+  const red = hex.substring(1, 3);
+  const green = hex.substring(3, 5);
+  const blue = hex.substring(5, 7);
+
+  return [red, green, blue].map((component) => parseInt(component, 16));
+};
+
+/**
+ * Construct an RGB hex string from individual red, green, blue integer values.
+ *
+ * @param {Array} Array of [red, green, blue] component values, each in range [0 255].
+ * @returns {string} RGB hex string for the specified input.
+ */
+export const rgbToHex = ([red, green, blue]) =>
+  `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
