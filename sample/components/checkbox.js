9182888e32af38e5268fac94ce9a0bae22293501
Kevin Lin <developer@kevinlin.info>
2017-12-25 18:23:30 -0800

    Change Checkbox to controlled component, update styling

--- sample/components/checkbox.js
+++ sample/components/checkbox.js
@@ -1,41 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Checkbox, Spacing, Text } from 'react-elemental';
 
-const SampleCheckbox = () => (
-  <div>
-    <Spacing size="huge" bottom>
-      <Text size="gamma" color="primary" uppercase>
-        Checkbox
-      </Text>
-      <Text>
-        Checkboxes denote opt-in choices controlled by the user.
-      </Text>
-    </Spacing>
+export default class SampleCheckbox extends Component {
+  state = {
+    enabled: true,
+    disabledChecked: true,
+  };
 
-    <Spacing size="huge" bottom>
-      <Spacing bottom>
-        <Text size="iota" color="gray70" uppercase bold>
-          Generic
-        </Text>
-      </Spacing>
+  handleChange = (key) => (isChecked) => this.setState({ [key]: isChecked });
 
-      <Spacing size="small" bottom>
-        <Checkbox label="Label" />
-      </Spacing>
-      <Spacing size="small" bottom>
-        <Checkbox label="Active" />
-      </Spacing>
-      <Spacing size="small" bottom>
-        <Checkbox label="Enabled" isChecked />
-      </Spacing>
-      <Spacing size="small" bottom>
-        <Checkbox label="Disabled state" disabled />
-      </Spacing>
-      <Spacing size="small" bottom>
-        <Checkbox label="Disabled, defaulted to checked" isChecked disabled />
-      </Spacing>
-    </Spacing>
-  </div>
-);
+  render() {
+    const { label, active, enabled, disabled, disabledChecked } = this.state;
 
-export default SampleCheckbox;
+    return (
+      <div>
+        <Spacing size="huge" bottom>
+          <Text size="gamma" color="primary" uppercase>
+            Checkbox
+          </Text>
+          <Text>
+            Checkboxes denote opt-in choices controlled by the user.
+          </Text>
+        </Spacing>
+
+        <Spacing size="huge" bottom>
+          <Spacing bottom>
+            <Text size="iota" color="gray70" uppercase bold>
+              Generic
+            </Text>
+          </Spacing>
+
+          <Spacing size="small" bottom>
+            <Checkbox
+              label="Label"
+              onChange={this.handleChange('label')}
+              checked={label}
+            />
+          </Spacing>
+
+          <Spacing size="small" bottom>
+            <Checkbox
+              label="Active"
+              onChange={this.handleChange('active')}
+              checked={active}
+            />
+          </Spacing>
+
+          <Spacing size="small" bottom>
+            <Checkbox
+              label="Enabled"
+              onChange={this.handleChange('enabled')}
+              checked={enabled}
+            />
+          </Spacing>
+
+          <Spacing size="small" bottom>
+            <Checkbox
+              label="Disabled state"
+              onChange={this.handleChange('disabled')}
+              checked={disabled}
+              disabled
+            />
+          </Spacing>
+
+          <Spacing size="small" bottom>
+            <Checkbox
+              label="Disabled, defaulted to checked"
+              onChange={this.handleChange('disabledChecked')}
+              checked={disabledChecked}
+              disabled
+            />
+          </Spacing>
+        </Spacing>
+      </div>
+    );
+  }
+}
--- test/components/checkbox.test.js
+++ test/components/checkbox.test.js
@@ -16,14 +16,6 @@ describe('Checkbox', () => {
     expect(checkbox.at(0).props().onClick).toBe(onClick);
   });
 
-  test('Setting ref in state', () => {
-    const checkbox = mount(
-      <Checkbox />,
-    );
-
-    expect(checkbox.state().ref).toBeTruthy();
-  });
-
   test('Standard rendering', () => {
     const checkbox = shallow(
       <Checkbox label="label" />,
@@ -34,63 +26,44 @@ describe('Checkbox', () => {
     expect(checkbox.find(Text).props().children).toBe('label');
   });
 
-  test('Check and uncheck actions', () => {
+  test('Rendering of checked checkbox', () => {
     const checkbox = shallow(
       <Checkbox />,
     );
 
-    expect(checkbox.state().isCurrentlyChecked).toBe(false);
-    expect(checkbox.instance().isChecked()).toBe(false);
-    checkbox.childAt(0).simulate('click');
-    expect(checkbox.state().isCurrentlyChecked).toBe(true);
-    expect(checkbox.instance().isChecked()).toBe(true);
-    checkbox.childAt(0).simulate('click');
-    expect(checkbox.state().isCurrentlyChecked).toBe(false);
-    expect(checkbox.instance().isChecked()).toBe(false);
+    expect(checkbox.find(Check).props().style.opacity).toBe(0);
   });
 
-  test('Keyboard actions', () => {
+  test('Check change callback for unchecked checkbox', () => {
+    const onChange = jest.fn();
     const checkbox = shallow(
-      <Checkbox />,
+      <Checkbox onChange={onChange} />,
     );
 
-    const checkContainer = checkbox.childAt(0).childAt(0);
+    expect(onChange.mock.calls.length).toBe(0);
+    checkbox.at(0).simulate('click');
+    expect(onChange).toBeCalledWith(true);
+  });
 
-    checkContainer.simulate('keydown', { keyCode: 32 });
-    expect(checkbox.instance().isChecked()).toBe(true);
-    checkContainer.simulate('keydown', { keyCode: 13 });
-    expect(checkbox.instance().isChecked()).toBe(false);
-    checkContainer.simulate('keydown', { keyCode: 27 });
-    expect(checkbox.instance().isChecked()).toBe(false);
-    checkContainer.simulate('keydown', { keyCode: 32 });
-    expect(checkbox.instance().isChecked()).toBe(true);
-    checkContainer.simulate('keydown', { keyCode: 27 });
-    expect(checkbox.instance().isChecked()).toBe(false);
-    checkContainer.simulate('keydown', { keyCode: 10 });
-    expect(checkbox.instance().isChecked()).toBe(false);
+  test('Uncheck change callback for checked checkbox', () => {
+    const onChange = jest.fn();
+    const checkbox = shallow(
+      <Checkbox onChange={onChange} checked />,
+    );
+
+    expect(onChange.mock.calls.length).toBe(0);
+    checkbox.at(0).simulate('click');
+    expect(onChange).toBeCalledWith(false);
   });
 
   test('Disabled checkbox', () => {
+    const onChange = jest.fn();
     const checkbox = shallow(
-      <Checkbox disabled />,
+      <Checkbox onChange={onChange} disabled />,
     );
 
-    expect(checkbox.instance().isChecked()).toBe(false);
-    checkbox.childAt(0).simulate('click');
-    expect(checkbox.instance().isChecked()).toBe(false);
-    checkbox.childAt(0).childAt(0).simulate('keydown', { keyCode: 32 });
-    expect(checkbox.instance().isChecked()).toBe(false);
-  });
-
-  test('Safe ref access', () => {
-    const checkbox = mount(
-      <Checkbox />,
-    );
-
-    const checkContainer = checkbox.childAt(0).childAt(0);
-
-    checkContainer.simulate('mouseout');
-    checkbox.setState({ ref: null });
-    checkContainer.simulate('mouseout');
+    expect(onChange.mock.calls.length).toBe(0);
+    checkbox.at(0).simulate('click');
+    expect(onChange.mock.calls.length).toBe(0);
   });
 });
