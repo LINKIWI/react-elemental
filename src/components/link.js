e51bcb1e352d2848e2a51e50ff9b3fc826aef6ed
Kevin Lin <developer@kevinlin.info>
2018-01-07 19:07:44 -0600

    Remove Link Radium dependency

--- src/components/link.js
+++ src/components/link.js
@@ -1,57 +1,95 @@
-import React from 'react';
-import Radium from 'radium';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import compose from 'util/compose';
+import withToggleState from 'util/with-toggle-state';
 
 /**
  * Styled link element.
  */
-export const BaseLink = ({
-  type,
-  activeColor,
-  href,
-  style: overrides,
-  children,
-  ...proxyProps
-}) => {
-  const borderSize = type === 'plain' ? '0' : '2px';
-  const style = {
-    color: 'currentColor',
-    opacity: 0.8,
-    textDecoration: 'none',
-    transition: '0.15s all ease',
-    ':hover': {
-      borderBottom: `${borderSize} solid currentColor`,
-      opacity: '1.0',
-    },
-    ':active': {
-      borderBottom: `${borderSize} solid currentColor`,
-      color: activeColor,
-    },
-    ...type === 'underline' && { borderBottom: '2px solid currentColor' },
-    ...overrides,
+class Link extends Component {
+  static propTypes = {
+    type: PropTypes.oneOf(['regular', 'plain', 'underline']),
+    href: PropTypes.string,
+    activeColor: PropTypes.string,
+    style: PropTypes.object,
+    children: PropTypes.node,
+    // HOC props
+    handleMouseOver: PropTypes.func.isRequired,
+    handleMouseOut: PropTypes.func.isRequired,
+    handleMouseDown: PropTypes.func.isRequired,
+    handleMouseUp: PropTypes.func.isRequired,
+    isHover: PropTypes.bool.isRequired,
+    isActive: PropTypes.bool.isRequired,
   };
 
-  return (
-    <a href={href} style={style} {...proxyProps}>
-      {children}
-    </a>
-  );
-};
+  static defaultProps = {
+    type: 'regular',
+    href: '#',
+    activeColor: 'currentColor',
+    style: {},
+    children: null,
+  };
 
-BaseLink.propTypes = {
-  type: PropTypes.oneOf(['regular', 'plain', 'underline']),
-  href: PropTypes.string,
-  activeColor: PropTypes.string,
-  style: PropTypes.object,
-  children: PropTypes.node,
-};
+  handleMouseOut = () => {
+    const { handleMouseOut, handleMouseUp } = this.props;
 
-BaseLink.defaultProps = {
-  type: 'regular',
-  href: '#',
-  activeColor: 'currentColor',
-  style: {},
-  children: null,
-};
+    // Also simulate a mouseup event so that the active state is properly reset if the user moves
+    // the mouse outside of the container without releasing the mouse.
+    handleMouseOut();
+    handleMouseUp();
+  };
 
-export default Radium(BaseLink);
+  render() {
+    const {
+      type,
+      activeColor,
+      href,
+      style: overrides,
+      children,
+      handleMouseOver,
+      handleMouseOut,
+      handleMouseDown,
+      handleMouseUp,
+      isHover,
+      isActive,
+      ...proxyProps
+    } = this.props;
+
+    const borderSize = type === 'plain' ? '0' : '2px';
+    const style = {
+      color: 'currentColor',
+      opacity: 0.8,
+      textDecoration: 'none',
+      transition: '0.15s all ease',
+      ...isHover && {
+        borderBottom: `${borderSize} solid currentColor`,
+        opacity: '1.0',
+      },
+      ...isHover && isActive && {
+        borderBottom: `${borderSize} solid currentColor`,
+        color: activeColor,
+      },
+      ...type === 'underline' && { borderBottom: '2px solid currentColor' },
+      ...overrides,
+    };
+
+    return (
+      <a
+        href={href}
+        style={style}
+        onMouseOver={handleMouseOver}
+        onMouseOut={this.handleMouseOut}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        {...proxyProps}
+      >
+        {children}
+      </a>
+    );
+  }
+}
+
+export default compose(
+  withToggleState({ key: 'isHover', enable: 'handleMouseOver', disable: 'handleMouseOut' }),
+  withToggleState({ key: 'isActive', enable: 'handleMouseDown', disable: 'handleMouseUp' }),
+)(Link);
--- test/components/link.test.js
+++ test/components/link.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { BaseLink as Link } from 'components/link';
+import { mount } from 'enzyme';
+import Link from 'components/link';
 
 describe('Link', () => {
   test('Standard rendering', () => {
-    const link = shallow(
+    const link = mount(
       <Link href="href">
         link
       </Link>,
@@ -12,34 +12,66 @@ describe('Link', () => {
 
     expect(link.find('a').length).toBe(1);
     expect(link.find('a').props().href).toBe('href');
-    expect(link.find('a').children().text()).toBe('link');
-    expect(link.find('a').props().style[':hover'].borderBottom.startsWith('2px solid')).toBe(true);
-    expect(link.find('a').props().style[':active'].borderBottom.startsWith('2px solid')).toBe(true);
+    expect(link.find('a').text()).toBe('link');
+  });
+
+  test('Regular type', () => {
+    const link = mount(
+      <Link activeColor="black">
+        link
+      </Link>,
+    );
+
+    expect(link.find('a').props().style.borderBottom).toBeUndefined();
+    expect(link.find('a').props().style.color).toBe('currentColor');
+    link.find('a').simulate('mouseover');
+    expect(link.find('a').props().style.borderBottom).toBe('2px solid currentColor');
+    link.find('a').simulate('mousedown');
+    expect(link.find('a').props().style.borderBottom).toBe('2px solid currentColor');
+    expect(link.find('a').props().style.color).toBe('black');
+    link.find('a').simulate('mouseup');
+    expect(link.find('a').props().style.color).toBe('currentColor');
   });
 
   test('Plain type', () => {
-    const link = shallow(
-      <Link type="plain">
+    const link = mount(
+      <Link type="plain" activeColor="black">
         link
       </Link>,
     );
 
-    expect(link.find('a').props().style[':active'].borderBottom.startsWith('0 solid')).toBe(true);
+    expect(link.find('a').props().style.borderBottom).toBeUndefined();
+    expect(link.find('a').props().style.color).toBe('currentColor');
+    link.find('a').simulate('mouseover');
+    expect(link.find('a').props().style.borderBottom).toBe('0 solid currentColor');
+    link.find('a').simulate('mousedown');
+    expect(link.find('a').props().style.borderBottom).toBe('0 solid currentColor');
+    expect(link.find('a').props().style.color).toBe('black');
+    link.find('a').simulate('mouseup');
+    expect(link.find('a').props().style.color).toBe('currentColor');
   });
 
   test('Underline type', () => {
-    const link = shallow(
-      <Link type="underline">
+    const link = mount(
+      <Link type="underline" activeColor="black">
         link
       </Link>,
     );
 
-    expect(link.find('a').props().style.borderBottom.startsWith('2px solid')).toBe(true);
+    expect(link.find('a').props().style.borderBottom).toBe('2px solid currentColor');
+    expect(link.find('a').props().style.color).toBe('currentColor');
+    link.find('a').simulate('mouseover');
+    expect(link.find('a').props().style.borderBottom).toBe('2px solid currentColor');
+    link.find('a').simulate('mousedown');
+    expect(link.find('a').props().style.borderBottom).toBe('2px solid currentColor');
+    expect(link.find('a').props().style.color).toBe('black');
+    link.find('a').simulate('mouseup');
+    expect(link.find('a').props().style.color).toBe('currentColor');
   });
 
   test('Props proxy', () => {
     const mockOnClick = jest.fn();
-    const link = shallow(
+    const link = mount(
       <Link href="href" onClick={mockOnClick} />,
     );
 
